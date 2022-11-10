b8f8b507c03ceb68e8d904445cadf8facd4d8ee0
Nikon Podgorny <podgorny.nikon@yandex.ru>
2022-11-10 15:36:16 +0300

    fix stuff

--- components/MassServiceSystem.tsx
+++ components/MassServiceSystem.tsx
@@ -9,7 +9,9 @@ import {
     XAxis,
     YAxis,
     CartesianGrid,
-    Bar
+    Bar,
+    ResponsiveContainer,
+    Tooltip
 } from "recharts";
 
 interface MSSProps {}
@@ -49,10 +51,24 @@ export default function MassServiceSystem({}: MSSProps) {
 
         let randomFunc = dist === "exp" ? getRandomExp : getRandomNorm;
 
-        let numbers = Array.from({ length: count }, () => randomFunc(avgT)); // [...Array(count)].map(() => randomFunc(avgT));
+        let numbers = new Array<number>(count);
+        for (let i = 0; i < count; i++) {
+            numbers[i] = randomFunc(avgT);
+        }
 
-        let maxVal = Math.max.apply(null, numbers);
-        let minVal = Math.min.apply(null, numbers);
+        // let numbers = Array.from({ length: count }, () => randomFunc(avgT)); // [...Array(count)].map(() => randomFunc(avgT));
+
+        let maxVal = -Infinity;
+        let minVal = Infinity;
+        let numbersLength = numbers.length;
+
+        for (let i = 0; i < numbersLength; i++) {
+            maxVal = Math.max(maxVal, numbers[i]);
+            minVal = Math.min(minVal, numbers[i]);
+        }
+
+        // let maxVal = Math.max.apply(null, numbers);
+        // let minVal = Math.min.apply(null, numbers);
 
         console.log("Interval count", intervalCount);
         console.log("min-max", minVal, maxVal);
@@ -137,19 +153,18 @@ export default function MassServiceSystem({}: MSSProps) {
             </div>
 
             {distPlot.length > 0 ? (
-                <div>
+                <ResponsiveContainer width="100%" height={400}>
                     <BarChart
-                        width={800}
-                        height={400}
                         data={distPlot.map((v) => {
                             return {
                                 value: v.count,
-                                name: Math.round(v.rangeEnd)
+                                name: v.rangeEnd.toFixed(2)
                             };
                         })}
                         barGap={0}
                         barCategoryGap={0}
                     >
+                        <Tooltip />
                         <XAxis dataKey="name" domain={["auto", "auto"]} />
                         <YAxis dataKey="value" domain={["auto", "auto"]} />
                         <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
@@ -160,7 +175,7 @@ export default function MassServiceSystem({}: MSSProps) {
                             maxBarSize={200}
                         />
                     </BarChart>
-                </div>
+                </ResponsiveContainer>
             ) : (
                 <div>
                     <Icon.ArrowUp /> Нажми сгенерировать
