dc7622d58845b32a1d86400d663a2591a355e5e2
Nikon Podgorny <podgorny.nikon@yandex.ru>
2022-11-17 16:30:47 +0300

    add home page

--- pages/dist.tsx
+++ pages/dist.tsx
@@ -2,23 +2,23 @@ import Link from "next/link";
 import React from "react";
 import MassServiceSystem from "../components/MassServiceSystem";
 import * as Icon from "react-feather";
-import Model from "../components/Model";
+import DistTest from "../components/DistTest";
 
-export default function Simulation() {
+export default function Dist() {
     return (
         <div className="container">
             <div style={{ display: "flex", justifyContent: "space-between" }}>
                 <Link className="button button-outline" href="/">
                     <Icon.Home size={16} /> Главная
                 </Link>
-                <Link className="button button-outline" href="/dist">
+                <Link className="button" href="/dist">
                     <Icon.BarChart2 size={16} /> Распределения
                 </Link>
-                <Link className="button" href="/simulation">
+                <Link className="button button-outline" href="/simulation">
                     <Icon.Cpu size={16} /> Симуляция
                 </Link>
             </div>
-            <Model></Model>
+            <DistTest></DistTest>
         </div>
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,13 +11,20 @@ export default function Home() {
 
             <div style={{ display: "flex", justifyContent: "space-between" }}>
                 <Link className="button" href="/">
+                    <Icon.Home size={16} /> Главная
+                </Link>
+                <Link className="button button-outline" href="/dist">
                     <Icon.BarChart2 size={16} /> Распределения
                 </Link>
                 <Link className="button button-outline" href="/simulation">
                     <Icon.Cpu size={16} /> Симуляция
                 </Link>
             </div>
-            <DistTest></DistTest>
+            <div>
+                <h1>Надежность и качество информационных систем</h1>
+                <h2>Лабораторная работа "Система массового обслуживания"</h2>
+                <blockquote>&copy; Никон Подгорный, ПетрГУ 22407</blockquote>
+            </div>
         </div>
     );
 }
